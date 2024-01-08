bd5345d1bfed428e2541a5707766eea9f5f40231
Apermtu22 <154979076+Apermtu22@users.noreply.github.com>
2024-01-09 01:23:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['255612851692', 'APERMTU', true],
+  ['6281255369012', 'Aper', true], 
   [''] 
 ] //Number of owners
 
