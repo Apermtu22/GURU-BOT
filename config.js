407c742a4af3320e27c3ad7dd0da7a8577652087
Apermtu22 <154979076+Apermtu22@users.noreply.github.com>
2024-01-09 01:29:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['255612851692', 'APERMTU', true],
-  ['6281255369012', 'Aper', true], 
+  ['255612851692', 'Aper', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Apermtu'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'Amadeus' 
+global.author = '@Apermtu' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
